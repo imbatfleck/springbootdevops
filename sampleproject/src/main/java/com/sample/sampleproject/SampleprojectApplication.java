dc3a471ccc831f666a73c8aeafcefc5e6b741390
Navin Rao <flash4navin@gmail.com>
2018-08-31 00:10:22 +0530

    com

--- sampleproject/src/main/java/com/sample/sampleproject/SampleprojectApplication.java
+++ sampleproject/src/main/java/com/sample/sampleproject/SampleprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleprojectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("NAvin");
 		SpringApplication.run(SampleprojectApplication.class, args);
 	}
 }
